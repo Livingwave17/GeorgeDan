fa3529aa6922c6964ae90104fd5de634c770e49a
Turbotwins2018 <Turbotwins2018@TURBO2018-PC>
2018-09-04 13:08:00 +0300

    FirstCommit

---
+++ JavaGeorgeDan/src/javageorgedan/JavaGeorgeDan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javageorgedan;
+
+/**
+ *
+ * @author Turbotwins2018
+ */
+public class JavaGeorgeDan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FirstCommit");// TODO code application logic here
+    }
+    
+}
